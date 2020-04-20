8381d24c7664e7508ca88adaae0c90da4528ce2c
David Villa <david.villa@kinesso.com>
2020-04-19 19:57:52 -0500

    Adding configs

--- main.go
+++ main.go
@@ -13,26 +13,20 @@ import (
 )
 
 func main() {
-	fmt.Println("First  Main!")
 	r := mux.NewRouter()
 	r.Use(app.JwtAuthentication)
-	port := os.Getenv("app_port")
+	port := os.Getenv("app_port") || os.Getenv("PORT")
 	if port == "" {
 		port = "8083" //localhost
 	}
-	fmt.Println("Second  Main!")
 	api := r.PathPrefix("/api/v1").Subrouter()
 	api.HandleFunc("/user/login", controllers.Authenticate).Methods("POST")
 	api.HandleFunc("/user/new", controllers.CreateAccount).Methods("POST")
 	api.HandleFunc("/students", controllers.GetAllStudents).Methods("GET")
 	api.HandleFunc("/students/{doc_num}", controllers.GetStudentByDocument).Methods("GET")
 	api.HandleFunc("/students/new", controllers.CreateStudent).Methods("POST")
-	fmt.Println("Third  Main!")
-	portd := os.Getenv("PORT")
-	err := http.ListenAndServe(":"+portd, r)
-	fmt.Println("Fourth  Main!")
+	err := http.ListenAndServe(":"+port, r)
 	if err != nil {
 		fmt.Print(err)
 	}
-	fmt.Println("Fifth  Main!")
 }
