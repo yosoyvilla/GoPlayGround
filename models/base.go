cf05e8ab48a2f4d0f08bde73098ec57f4b477ff2
David Villa <david.villa@kinesso.com>
2020-04-19 19:04:48 -0500

    Adding configs

--- models/base.go
+++ models/base.go
@@ -24,16 +24,16 @@ func init() {
 	dbHost := os.Getenv("db_host")
 	dbPort := os.Getenv("db_port")
 	dBType := os.Getenv("db_type")
-	dbUri = ""
+	dbURI := ""
 
 	if dBType == "mysql" {
-		dbUri := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", username, password, dbHost, dbPort, dbName) //Build connection string
+		dbURI = fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", username, password, dbHost, dbPort, dbName) //Build connection string
 	} else {
-		dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+		dbURI = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
 	}
-	fmt.Println(dbUri)
+	fmt.Println(dbURI)
 
-	conn, err := gorm.Open(dBType, dbUri)
+	conn, err := gorm.Open(dBType, dbURI)
 	if err != nil {
 		fmt.Print(err)
 	}
